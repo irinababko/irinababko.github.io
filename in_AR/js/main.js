6ec5a27b33fa117d02f0d9f345a4ad43c61c605a
irinababko <32390331+irinababko@users.noreply.github.com>
2018-09-25 10:25:54 +0300

    Add files via upload

---
+++ in_AR/js/main.js
@@ -0,0 +1,121 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+
+
+var typed = new Typed('.text', {
+    strings: ["open the door into the future of your business"],
+    typeSpeed: 50
+});
+
+window.FontAwesomeConfig = {
+    searchPseudoElements: true
+}
+// var colors = new Array(
+//     [62,35,255],
+//     [60,255,60],
+//     [255,35,98],
+//     [45,175,230],
+//     [255,0,255],
+//     [255,128,0]);
+// var colors = new Array(
+//     [3,11,52],
+//     [34,60,196],
+//     [84,109,235],
+//     [79,144,236],
+//     [79,144,236],
+//     [79,144,236],
+//     [45,81,214]);
+var colors = new Array(
+    [3,11,52],
+    [3,11,52],
+    [3,11,52],
+    [84,109,235],
+    [84,109,235],
+    [84,109,235]);
+// var colors = new Array(
+//     [46,102,255],
+//     [46,102,255],
+//     [46,102,255],
+//     [82,234,255],
+//     [82,234,255],
+//     [82,234,255]);
+
+
+var step = 0;
+//color table indices for:
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient()
+{
+
+    if ( $===undefined ) return;
+
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
+
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+    $('#gradient').css({
+        background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+        background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+
+
+    step += gradientSpeed;
+    if ( step >= 1 )
+    {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
+
+        //pick two new target color indices
+        //do not pick the same as the current one
+        colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+    }
+}
+
+setInterval(updateGradient,10);
+

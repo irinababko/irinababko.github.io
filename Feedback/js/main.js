7d434b3c5adbee18b81d9abada7317a867a05d38
irinababko <32390331+irinababko@users.noreply.github.com>
2018-12-03 05:05:18 +0200

    Add files via upload

--- Feedback/js/main.js
+++ Feedback/js/main.js
@@ -1,13 +1,16 @@
 
 $( document ).ready(function() {
-    $('.reply').click(function () {
-        $('.reply-open').toggleClass('open');
+    $('.reply0').click(function () {
+        $('.reply-open0').toggleClass('open');
     });
     $('.reply1').click(function () {
         $('.reply-open1').toggleClass('open');
     });
     $('.reply2').click(function () {
         $('.reply-open2').toggleClass('open');
+    });
+    $('.reply3').click(function () {
+        $('.reply-open3').toggleClass('open');
     });
 });
 
